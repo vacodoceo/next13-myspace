54fc551bcffd0549f287b6ec2abdfdc815d0784b
Verner Codoceo <vacodoceo@uc.cl>
2023-05-10 20:43:07 -0400

    🛂 add prisma adapter to auth js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
+import { authOptions } from "./api/auth/[...nextauth]/route";
 
 const Home = async () => {
-  const session = await getServerSession();
-
-  console.log({ session });
+  const session = await getServerSession(authOptions);
 
   if (!session) {
     redirect("/api/auth/signin");
