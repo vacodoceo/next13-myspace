4df753f8e9dc803ece9edeb50cd7f7f4c611b7ff
Verner Codoceo <vacodoceo@uc.cl>
2023-05-12 15:28:07 -0400

    💬 greet in japanese

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -1,5 +1,7 @@
 "use client";
 
 export const Header = () => (
-  <h1 className="text-3l">Hola desde {process.env.NEXT_PUBLIC_ENVIRONMENT}</h1>
+  <h1 className="text-3l">
+    こんにちは desde {process.env.NEXT_PUBLIC_ENVIRONMENT}
+  </h1>
 );
